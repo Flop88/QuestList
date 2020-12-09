ef6c55ab8c64f06bcfd2abd3e465ba36db726ec2
Maxim <wwwqwwwqwwwqwwwq@gmail.com>
2020-12-09 12:19:33 +0300

    Create Project, add dependencies

---
+++ app/src/main/java/ru/mvlikhachev/questlist/screens/QuestListViewModel.kt
@@ -0,0 +1,11 @@
+package ru.mvlikhachev.questlist.screens
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class QuestListViewModel(application: Application) : AndroidViewModel(application) {
+
+    fun fetchQuestList() {
+
+    }
+}
